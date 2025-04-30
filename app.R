af0ef4e704a37470e0cc29d7e4a292f1ce1122e9
jventural <jventuraleon@gmail.com>
2025-04-30 06:10:22 -0500

    update

--- app.R
+++ app.R
@@ -261,9 +261,9 @@ server <- function(input, output, session) {
                 size  = 5) +                                   # tamaño de la etiqueta
       geom_col(fill="#713f8e")+coord_flip()+theme_minimal()+
       theme(
-        axis.text.x = element_text(size = 14),  # tamaño texto eje X
-        axis.text.y = element_text(size = 14),  # tamaño texto eje Y
-        axis.title  = element_text(size = 16)   # tamaño títulos de ejes si los tuvieras
+        axis.text.x = element_text(size = 12),  # tamaño texto eje X
+        axis.text.y = element_text(size = 12),  # tamaño texto eje Y
+        axis.title  = element_text(size = 14)   # tamaño títulos de ejes si los tuvieras
       ) +
       labs(x=NULL,y=NULL)
   })
